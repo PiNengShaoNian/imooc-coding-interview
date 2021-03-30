512279dbe963fdfb1ca976a54262907de44d7f0e
laoqu <quguiyou1076@qq.com>
2021-03-30 21:41:21 +0800

    荷兰旗问题

--- src/chapter4-array/__test__/sort-colors.test.ts
+++ src/chapter4-array/__test__/sort-colors.test.ts
@@ -1,9 +1,13 @@
-import { sortColors } from '../sort-colors'
+import { sortColors, sortColors1 } from '../sort-colors'
 
 test('sortColors', () => {
   const arr1 = [2, 0, 2, 1, 1, 0]
+  const arr2 = [2, 0, 2, 1, 1, 0]
 
   sortColors(arr1)
 
+  sortColors1(arr2)
+
   expect(arr1).toEqual([0, 0, 1, 1, 2, 2])
+  expect(arr2).toEqual([0, 0, 1, 1, 2, 2])
 })
