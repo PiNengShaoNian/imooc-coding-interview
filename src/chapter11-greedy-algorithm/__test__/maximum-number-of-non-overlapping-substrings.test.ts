7f0fd5adfb735f5c7d493cfd7da3a8820d2f449c
瞿贵友 <quguiyou1076@qq.com>
2021-06-18 20:20:31 +0800

    贪心算法7

--- src/chapter11-greedy-algorithm/__test__/maximum-number-of-non-overlapping-substrings.test.ts
+++ src/chapter11-greedy-algorithm/__test__/maximum-number-of-non-overlapping-substrings.test.ts
@@ -1,7 +1,14 @@
-import { maxNumOfSubstrings } from '../maximum-number-of-non-overlapping-substrings'
+import {
+  maxNumOfSubstrings,
+  maxNumOfSubstrings1,
+} from '../maximum-number-of-non-overlapping-substrings'
 
 test('maxNumOfSubstrings', () => {
   expect(maxNumOfSubstrings('adefaddaccc')).toEqual(['e', 'f', 'ccc'])
   expect(maxNumOfSubstrings('abbaccd')).toEqual(['bb', 'cc', 'd'])
   expect(maxNumOfSubstrings('a')).toEqual(['a'])
+  
+  expect(maxNumOfSubstrings1('adefaddaccc')).toEqual(['e', 'f', 'ccc'])
+  expect(maxNumOfSubstrings1('abbaccd')).toEqual(['bb', 'cc', 'd'])
+  expect(maxNumOfSubstrings1('a')).toEqual(['a'])
 })
--- src/chapter11-greedy-algorithm/maximum-number-of-non-overlapping-substrings.ts
+++ src/chapter11-greedy-algorithm/maximum-number-of-non-overlapping-substrings.ts
@@ -36,3 +36,73 @@ export const maxNumOfSubstrings = (s: string): string[] => {
 
   return ans
 }
+
+/**
+ * 前缀和解法
+ * @param s
+ * @returns
+ */
+export const maxNumOfSubstrings1 = (s: string): string[] => {
+  const presum: number[][] = Array.from({ length: 26 }, () => [0])
+  const range: number[][] = Array.from({ length: 26 }, () => [
+    Infinity,
+    -Infinity,
+  ])
+
+  for (let i = 0; i < s.length; ++i) {
+    const index = s.charCodeAt(i) - 97
+
+    range[index][0] = Math.min(range[index][0], i)
+    range[index][1] = Math.max(range[index][1], i)
+    for (let j = 0; j < 26; ++j) {
+      presum[j][i + 1] = presum[j][i]
+
+      if (j === index) {
+        ++presum[j][i + 1]
+      }
+    }
+  }
+
+  const expand: number[][] = Array.from({ length: 26 }, () => [
+    Infinity,
+    -Infinity,
+  ])
+
+  for (let i = 0; i < 26; ++i) {
+    expand[i][0] = range[i][0]
+    expand[i][1] = range[i][1]
+
+    if (range[i][0] > range[i][1]) continue
+
+    while (true) {
+      const l = expand[i][0]
+      const r = expand[i][1]
+      for (let j = 0; j < 26; ++j) {
+        if (presum[j][r + 1] - presum[j][l] > 0) {
+          expand[i][0] = Math.min(range[j][0], expand[i][0])
+          expand[i][1] = Math.max(range[j][1], expand[i][1])
+        }
+      }
+
+      if (l === expand[i][0] && r === expand[i][1]) break
+    }
+  }
+
+  expand.sort((a, b) => a[1] - b[1])
+
+  let end = -1
+  const ans: string[] = []
+
+  for (let i = 0; i < 26; ++i) {
+    const l = expand[i][0]
+    const r = expand[i][1]
+
+    if (l > r) continue
+    if (end <= l) {
+      ans.push(s.substring(l, r + 1))
+      end = r
+    }
+  }
+
+  return ans
+}
