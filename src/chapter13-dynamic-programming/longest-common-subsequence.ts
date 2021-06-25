5064fc4b6318b7334b0e443054225c96701d8ef2
laoqu <quguiyou1076@qq.com>
2021-06-25 23:35:13 +0800

    动态规划2

---
+++ src/chapter13-dynamic-programming/longest-common-subsequence.ts
@@ -0,0 +1,20 @@
+export function longestCommonSubsequence(text1: string, text2: string): number {
+  const m = text1.length
+  const n = text2.length
+
+  const dp: number[][] = Array.from({ length: m + 1 }, () =>
+    Array.from({ length: n + 1 }, () => 0)
+  )
+
+  for (let i = 1; i <= m; ++i) {
+    for (let j = 1; j <= n; ++j) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j])
+      }
+    }
+  }
+
+  return dp[m][n]
+}
